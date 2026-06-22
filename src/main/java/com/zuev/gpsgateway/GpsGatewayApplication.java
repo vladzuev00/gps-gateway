b7677bb61c6086ed5893b73eb4f8564117b4bd4c
Vlad <v.zuev@besmart.by>
2026-06-22 14:02:41 +0300

    correct GpsGatewayApplication

--- src/main/java/com/zuev/gpsgateway/GpsGatewayApplication.java
+++ src/main/java/com/zuev/gpsgateway/GpsGatewayApplication.java
@@ -1,12 +1,13 @@
 package com.zuev.gpsgateway;
 
-import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+import static org.springframework.boot.SpringApplication.run;
 
 @SpringBootApplication
 public class GpsGatewayApplication {
 
     public static void main(String[] args) {
-        SpringApplication.run(GpsGatewayApplication.class, args);
+        run(GpsGatewayApplication.class, args);
     }
 }
