37e612fa23a1c4dcfdde7ad3a83003f4c8b3ab3a
VLAD-PC <vladzuev18022000@gmail.com>
2026-07-26 09:23:38 +0300

    refactor MangoMessageDecoder and MangoMessageDecoderTest

--- src/test/java/com/zuev/gpsgateway/decoder/mango/pkg/MangoMessageDecoderTest.java
+++ src/test/java/com/zuev/gpsgateway/decoder/mango/pkg/MangoMessageDecoderTest.java
@@ -1,6 +1,6 @@
 package com.zuev.gpsgateway.decoder.mango.pkg;
 
-import com.zuev.gpsgateway.model.mango.MangoData;
+import com.zuev.gpsgateway.model.mango.MangoMessage;
 import io.netty.buffer.ByteBuf;
 import org.junit.jupiter.api.Test;
 
@@ -8,15 +8,15 @@ import static io.netty.buffer.ByteBufUtil.decodeHexDump;
 import static io.netty.buffer.Unpooled.wrappedBuffer;
 import static org.junit.jupiter.api.Assertions.assertEquals;
 
-public final class MangoDataDecoderTest {
-    private final MangoDataDecoder decoder = new MangoDataDecoder();
+public final class MangoMessageDecoderTest {
+    private final MangoMessageDecoder decoder = new MangoMessageDecoder();
 
     @Test
-    public void dataWithOptionalFieldsShouldBeRead() {
+    public void messageWithOptionalFieldsShouldBeDecode() {
         ByteBuf givenByteBuf = wrappedBuffer(decodeHexDump("0000018bcfe56800404a0fcd67fd3f5b40107e6b3fe9fadb7f003c00b441280000083f99999a01550000018bcfe56be8404a53c0ca600b0340129e065300581500"));
 
-        MangoData actual = decoder.read(givenByteBuf);
-        MangoData expected = new MangoData(
+        MangoMessage actual = decoder.decode(givenByteBuf);
+        MangoMessage expected = new MangoMessage(
                 1700000000000L,
                 52.123456,
                 4.123456,
@@ -33,11 +33,11 @@ public final class MangoDataDecoderTest {
     }
 
     @Test
-    public void dataWithoutOptionalFieldsShouldBeRead() {
+    public void messageWithoutOptionalFieldsShouldBeDecode() {
         ByteBuf givenByteBuf = wrappedBuffer(decodeHexDump("0000018bcfe56800404a0fcd67fd3f5b40107e6b3fe9fadb000000018bcfe56be8404a53c0ca600b0340129e06530058157f003c00b441280000083f99999a0155"));
 
-        MangoData actual = decoder.read(givenByteBuf);
-        MangoData expected = new MangoData(
+        MangoMessage actual = decoder.decode(givenByteBuf);
+        MangoMessage expected = new MangoMessage(
                 1700000000000L,
                 52.123456,
                 4.123456,
