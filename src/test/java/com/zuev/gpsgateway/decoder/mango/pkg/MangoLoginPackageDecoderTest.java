969ce8f158cb76ce70bd5ef70f785eb7132f83aa
VLAD-PC <vladzuev18022000@gmail.com>
2026-07-26 09:21:13 +0300

    refactor MangoLoginPackageDecoder and MangoLoginPackageDecoderTest

--- src/test/java/com/zuev/gpsgateway/decoder/mango/pkg/MangoLoginPackageDecoderTest.java
+++ src/test/java/com/zuev/gpsgateway/decoder/mango/pkg/MangoLoginPackageDecoderTest.java
@@ -1,6 +1,6 @@
 package com.zuev.gpsgateway.decoder.mango.pkg;
 
-import com.zuev.gpsgateway.model.mango.MangoAuthPackage;
+import com.zuev.gpsgateway.model.mango.MangoLoginPackage;
 import io.netty.buffer.ByteBuf;
 import org.junit.jupiter.api.Test;
 
@@ -8,15 +8,15 @@ import static io.netty.buffer.ByteBufUtil.decodeHexDump;
 import static io.netty.buffer.Unpooled.wrappedBuffer;
 import static org.junit.jupiter.api.Assertions.assertEquals;
 
-public final class MangoAuthPackageDecoderTest {
-    private final MangoAuthPackageDecoder decoder = new MangoAuthPackageDecoder();
+public final class MangoLoginPackageDecoderTest {
+    private final MangoLoginPackageDecoder decoder = new MangoLoginPackageDecoder();
 
     @Test
     public void payloadShouldBeDecoded() {
         ByteBuf givenPayload = wrappedBuffer(decodeHexDump("3535353535353535353535353535350474657374"));
 
-        MangoAuthPackage actual = decoder.decodePayload(givenPayload);
-        MangoAuthPackage expected = new MangoAuthPackage("555555555555555", "test");
+        MangoLoginPackage actual = decoder.decodePayload(givenPayload);
+        MangoLoginPackage expected = new MangoLoginPackage("555555555555555", "test");
         assertEquals(expected, actual);
         assertEquals(0, givenPayload.readableBytes());
     }
