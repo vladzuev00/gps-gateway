e26752d503f567253efe73f67d4d70ec408e457c
VLAD-PC <vladzuev18022000@gmail.com>
2026-08-08 18:10:43 +0300

    refactor MangoMessageDecoder

--- src/main/java/com/zuev/gpsgateway/decoder/mango/pkg/MangoMessageDecoder.java
+++ src/main/java/com/zuev/gpsgateway/decoder/mango/pkg/MangoMessageDecoder.java
@@ -6,26 +6,26 @@ import org.springframework.stereotype.Component;
 
 @Component
 public final class MangoMessageDecoder {
-    private static final byte SPEED_PRESENT_FLAG = 0x01;
-    private static final byte COURSE_PRESENT_FLAG = 0x02;
-    private static final byte ALTITUDE_PRESENT_FLAG = 0x04;
-    private static final byte SATELLITE_COUNT_PRESENT_FLAG = 0x08;
-    private static final byte HDOP_PRESENT_FLAG = 0x10;
-    private static final byte IGNITION_PRESENT_FLAG = 0x20;
-    private static final byte BATTERY_PRESENT_FLAG = 0x40;
+    private static final byte SPEED_PRESENCE_MASK = 0x01;
+    private static final byte COURSE_PRESENCE_MASK = 0x02;
+    private static final byte ALTITUDE_PRESENCE_MASK = 0x04;
+    private static final byte SATELLITE_COUNT_PRESENCE_MASK = 0x08;
+    private static final byte HDOP_PRESENCE_MASK = 0x10;
+    private static final byte IGNITION_PRESENCE_MASK = 0x20;
+    private static final byte BATTERY_PRESENCE_MASK = 0x40;
 
     public MangoMessage decode(ByteBuf byteBuf) {
         long epochMillis = byteBuf.readLong();
         double latitude = byteBuf.readDouble();
         double longitude = byteBuf.readDouble();
         byte presenceBitMask = byteBuf.readByte();
-        Short speed = (presenceBitMask & SPEED_PRESENT_FLAG) != 0 ? byteBuf.readShort() : null;
-        Short course = (presenceBitMask & COURSE_PRESENT_FLAG) != 0 ? byteBuf.readShort() : null;
-        Float altitude = (presenceBitMask & ALTITUDE_PRESENT_FLAG) != 0 ? byteBuf.readFloat() : null;
-        Byte satelliteCount = (presenceBitMask & SATELLITE_COUNT_PRESENT_FLAG) != 0 ? byteBuf.readByte() : null;
-        Float hdop = (presenceBitMask & HDOP_PRESENT_FLAG) != 0 ? byteBuf.readFloat() : null;
-        Byte ignition = (presenceBitMask & IGNITION_PRESENT_FLAG) != 0 ? byteBuf.readByte() : null;
-        Byte battery = (presenceBitMask & BATTERY_PRESENT_FLAG) != 0 ? byteBuf.readByte() : null;
+        Short speed = (presenceBitMask & SPEED_PRESENCE_MASK) != 0 ? byteBuf.readShort() : null;
+        Short course = (presenceBitMask & COURSE_PRESENCE_MASK) != 0 ? byteBuf.readShort() : null;
+        Float altitude = (presenceBitMask & ALTITUDE_PRESENCE_MASK) != 0 ? byteBuf.readFloat() : null;
+        Byte satelliteCount = (presenceBitMask & SATELLITE_COUNT_PRESENCE_MASK) != 0 ? byteBuf.readByte() : null;
+        Float hdop = (presenceBitMask & HDOP_PRESENCE_MASK) != 0 ? byteBuf.readFloat() : null;
+        Byte ignition = (presenceBitMask & IGNITION_PRESENCE_MASK) != 0 ? byteBuf.readByte() : null;
+        Byte battery = (presenceBitMask & BATTERY_PRESENCE_MASK) != 0 ? byteBuf.readByte() : null;
         return new MangoMessage(epochMillis, latitude, longitude, speed, course, altitude, satelliteCount, hdop, ignition, battery);
     }
 }
