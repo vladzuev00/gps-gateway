064eb6aff65780afd21d00ce6203865a340ac830
Vlad <v.zuev@besmart.by>
2026-08-12 11:39:30 +0300

    refactor MangoProtocolDecoder

--- src/main/java/com/zuev/gpsgateway/decoder/mango/MangoProtocolDecoder.java
+++ src/main/java/com/zuev/gpsgateway/decoder/mango/MangoProtocolDecoder.java
@@ -8,11 +8,11 @@ import java.util.List;
 import java.util.OptionalInt;
 
 public final class MangoProtocolDecoder extends BinaryProtocolDecoder {
-    private static final int PACKAGE_PREFIX_LENGTH = 2;
-    private static final int PACKAGE_TYPE_LENGTH = 1;
+    private static final int PREFIX_LENGTH = 2;
+    private static final int TYPE_LENGTH = 1;
     private static final int PAYLOAD_LENGTH_LENGTH = 2;
     private static final int CHECKSUM_LENGTH = 2;
-    private static final int EMPTY_PACKAGE_LENGTH = PACKAGE_PREFIX_LENGTH + PACKAGE_TYPE_LENGTH + PAYLOAD_LENGTH_LENGTH
+    private static final int EMPTY_PACKAGE_LENGTH = PREFIX_LENGTH + TYPE_LENGTH + PAYLOAD_LENGTH_LENGTH
             + CHECKSUM_LENGTH;
 
     public MangoProtocolDecoder(List<MangoPackageDecoder> packageDecoders) {
@@ -24,7 +24,7 @@ public final class MangoProtocolDecoder extends BinaryProtocolDecoder {
         if (byteBuf.readableBytes() < EMPTY_PACKAGE_LENGTH) {
             return OptionalInt.empty();
         }
-        int payloadLength = byteBuf.getUnsignedShort(byteBuf.readerIndex() + PACKAGE_PREFIX_LENGTH + PACKAGE_TYPE_LENGTH);
+        int payloadLength = byteBuf.getUnsignedShort(byteBuf.readerIndex() + PREFIX_LENGTH + TYPE_LENGTH);
         int completePackageLength = EMPTY_PACKAGE_LENGTH + payloadLength;
         if (byteBuf.readableBytes() < completePackageLength) {
             return OptionalInt.empty();
@@ -40,7 +40,7 @@ public final class MangoProtocolDecoder extends BinaryProtocolDecoder {
     @Override
     protected int calculateChecksum(ByteBuf byteBuf) {
         int sum = 0;
-        for (int i = byteBuf.readerIndex() + PACKAGE_PREFIX_LENGTH; i < byteBuf.writerIndex() - CHECKSUM_LENGTH; i++) {
+        for (int i = byteBuf.readerIndex() + PREFIX_LENGTH; i < byteBuf.writerIndex() - CHECKSUM_LENGTH; i++) {
             sum += byteBuf.getUnsignedByte(i);
         }
         return sum & 0xFFFF;
